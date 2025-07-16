106a9ab18a70b6b6b5e89237d4bc2e540ed464b7
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-07-16 18:28:46 +0200

    take the prompt from outut file

--- extractor_glosas_1_imagen.py
+++ extractor_glosas_1_imagen.py
@@ -1,6 +1,18 @@
 from google import genai
 from google.genai import types
 
+
+# Ruta del archivo PDF
+pdf_path = 'data/Gramatica-Normativa-Kiche_PAG_20.pdf'
+# Ruta del archivo de texto que contiene el prompt
+prompt_path = 'prompts/prompt_v1'  # Cambia esto por la ruta correcta
+
+# Leer el prompt desde el archivo txt
+with open(prompt_path, 'r', encoding='utf-8') as f:
+    prompt = f.read().strip()  # strip() elimina espacios en blanco y saltos de línea al inicio y final
+
+print (f"Prompt leído: {prompt}")
+
 client = genai.Client()
 
 path = 'data/Gramatica-Normativa-Kiche_PAG_20.pdf'
@@ -15,7 +27,7 @@ contents=[
     data=image_bytes,
     mime_type='application/pdf',
     ),
-    'Caption this image.'
+    prompt
 ]
 )
 
