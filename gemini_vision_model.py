4959fc57c030174ed74856d780d14a8059ee063f
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-07-16 17:59:32 +0200

    Initial commit

---
+++ gemini_vision_model.py
@@ -0,0 +1,22 @@
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+path = 'data/Gramatica-Normativa-Kiche_pag_41.pdf'
+
+with open(path, 'rb') as f:
+    image_bytes = f.read()
+
+response = client.models.generate_content(
+model='gemini-2.5-flash',
+contents=[
+    types.Part.from_bytes(
+    data=image_bytes,
+    mime_type='application/pdf',
+    ),
+    'Caption this image.'
+]
+)
+
+print(response.text)
---
+++ test.py
@@ -0,0 +1,4 @@
+import google.generativeai as genai
+
+print("¡Paquete importado correctamente!")
+print(f"Versión instalada: {genai.__version__}")
