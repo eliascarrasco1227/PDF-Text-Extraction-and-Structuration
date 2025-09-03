1a4befd98d5ef2c28938a8712a9eeaf7d9536a7f
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-09-03 12:08:27 +0200

    mejorado sistema de páginas

--- config/paths.py
+++ config/paths.py
@@ -2,5 +2,5 @@
 PDF_PATH = 'data/Gramatica-Normativa-Mam.pdf'
 PROMPT_PATH = 'prompts/prompt_v3'
 OUTPUT_DIR = 'output'
-PAGINAS = (20, 35)  # Páginas a procesar (inicio, fin), ambas inclusive.  (pagina = las que indica el pdf - 1)
+PAGINAS = (20, 35)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
 PAGES_PER_BLOCK = 5  # Número de páginas a procesar por bloque
