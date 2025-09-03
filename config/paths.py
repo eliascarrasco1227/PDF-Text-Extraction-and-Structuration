d7c2e2ff1c23f27924dd83dc7aa8661a59fa1060
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-09-03 15:10:38 +0200

    comienzo del desarrollo del test set y mejora del prompt

--- config/paths.py
+++ config/paths.py
@@ -1,7 +1,7 @@
 # Configuración editable
-PDF_PATH = 'data/Gramatica-Normativa-Kiche-1-4.pdf'
+PDF_PATH = 'data/Gramatica-Normativa-Kaqchikel.pdf'
 PROMPT_PATH = 'prompts/prompt_v3'
 OUTPUT_DIR = 'output'
 ALL_PAGES = False  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
-PAGINAS = (1, 4)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
+PAGINAS = (146, 146)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
 PAGES_PER_BLOCK = 5  # Número de páginas a procesar por bloque
