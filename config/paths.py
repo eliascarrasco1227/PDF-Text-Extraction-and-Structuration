135afc80e717934d3008cb1a463de1e65a357906
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-09-11 00:11:49 +0200

    generadas páginas para comparar con el test set

--- config/paths.py
+++ config/paths.py
@@ -1,7 +1,7 @@
 # Configuración editable
-PDF_PATH = 'data/Gramatica-Normativa-Mam.pdf'
+PDF_PATH = 'data/Gramatica-Normativa-Kiche.pdf'
 PROMPT_PATH = 'prompts/prompt_v3'
 OUTPUT_DIR = 'output'
 ALL_PAGES = False  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
-PAGINAS = (80, 80)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
+PAGINAS = (43, 43)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
 PAGES_PER_BLOCK = 5  # Número de páginas a procesar por bloque
