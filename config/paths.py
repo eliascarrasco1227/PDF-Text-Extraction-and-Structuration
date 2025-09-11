6bc0406f8e8da4dc43f954bedbf532b99f2d80ef
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-09-11 16:46:25 +0200

    pruebas para test set

--- config/paths.py
+++ config/paths.py
@@ -2,7 +2,7 @@
 PDF_PATH = 'data/Gramatica-Normativa-Kiche.pdf'
 PROMPT_PATH = 'prompts/prompt_v3'
 OUTPUT_DIR = 'output'
-LOG_DIR = 'logs'  # Nueva variable para directorio de logs
+LOG_DIR = 'logs'  # directorio de logs
 ALL_PAGES = False  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
 PAGINAS = (43, 43)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
 PAGES_PER_BLOCK = 5  # Número de páginas a procesar por bloque
