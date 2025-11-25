bd3f663c85aaa801bf9d4ad0910d24a2764687de
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2025-11-25 18:38:46 +0100

    kiche examples with zero shot

--- config/properties.py
+++ config/properties.py
@@ -1,12 +1,13 @@
 # Configuración editable
 #PDF_PATH = 'data/Gramatica-Normativa-Mam.pdf'
 #PDF_PATH = 'data/few-shot/Gramatica-Normativa-Kaqchikel_pag_146.pdf'
-PDF_PATH = 'data/reducidos/Gramatica-Normativa-Kaqchikel_pag_172.pdf'
+#PDF_PATH = 'data/reducidos/Gramatica-Normativa-Kaqchikel_pag_172.pdf'
+PDF_PATH = 'data/Gramatica-Normativa-Kiche.pdf'
 PROMPT_PATH = 'prompts/prompt_v4'
 OUTPUT_DIR = 'output'
 LOG_DIR = 'logs'  # directorio de logs
-ALL_PAGES = True  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
-PAGINAS = (172, 172)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
+ALL_PAGES = False  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
+PAGINAS = (43, 43)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
 PAGES_PER_BLOCK = 5  # Número de páginas a procesar por bloque
 
 # Configuración para reintentos
@@ -19,7 +20,7 @@ FEW_SHOT_PDF_PATH = 'data/few-shot/Gramatica-Normativa-Kaqchikel_pag_146.pdf'
 FEW_SHOT_XML_PATH = 'data/few-shot/Gramatica-Normativa-Kaqchikel_páginas_146-146.xml'
 
 # Interruptor para activar/desactivar el few-shot si quieres probar sin él
-USE_FEW_SHOT = False
+USE_FEW_SHOT = True
 
 # Evaluator
 # Las variables de evaluator están en evaluator/properties.py
