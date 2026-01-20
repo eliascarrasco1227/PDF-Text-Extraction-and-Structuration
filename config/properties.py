d85c05971687ea7c4789aaf6200c755a955f4c85
eliascarrasco1227 <eliascarrasco1227@gmail.com>
2026-01-20 18:46:50 +0100

    dtd actualizado y quitamos titulo del prompt

--- config/properties.py
+++ config/properties.py
@@ -1,15 +1,16 @@
 # Configuración editable
 #PDF_PATH = 'data/Gramatica-Normativa-Mam.pdf'
 #PDF_PATH = 'data/few-shot/Gramatica-Normativa-Kaqchikel_pag_146.pdf'
-PDF_PATH = 'data/reducidos/Gramatica-Normativa-Kaqchikel_pag_172.pdf'
+#PDF_PATH = 'data/reducidos/Gramatica-Normativa-Kaqchikel_pag_172.pdf'
+PDF_PATH = 'data/Gramatica-Normativa-Kaqchikel.pdf'
 #PDF_PATH = 'data/Gramatica-Normativa-Kaqchikel.pdf'
 #PDF_PATH = 'data/Gramatica-Normativa-Kiche.pdf'
 #PDF_PATH = 'data/reducidos/Gramatica-Normativa-Kiche_pag_44.pdf'
 PROMPT_PATH = 'prompts/prompt_v4'
 OUTPUT_DIR = 'output'
 LOG_DIR = 'logs'  # directorio de logs
-ALL_PAGES = True  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
-PAGINAS = (146, 146)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
+ALL_PAGES = False  # Si es True, procesa todas las páginas del PDF ignorando PAGINAS
+PAGINAS = (160, 180)  # Páginas a procesar (inicio, fin), ambas inclusive.  (primera página = 0. pagina = las que indica el pdf - 1)
 PAGES_PER_BLOCK = 5  # Número de páginas a procesar por bloque
 
 # Configuración para reintentos
@@ -32,7 +33,7 @@ FEW_SHOT_XML_PATH_2 = 'data/few-shot/Gramatica-Normativa-Mam_páginas_80-80.xml'
 # Temperatura: 0.0 = Deterministico (Mejor para extracción fiel). 1.0 = Creativo. (default in gemini 1.0, balanced) 
 TEMPERATURE = 0.25
 
-GEMINI_MODEL = 'gemini-2.5-flash' #'gemma-3-27b-it'  # Modelo de Gemini a usar
+GEMINI_MODEL = 'gemini-2.5-flash-lite' #'gemma-3-27b-it'  # Modelo de Gemini a usar
 
 
 # Validator
